98a591e05d4f407e28393dbfafddb7440fb19612
Rajommkar <marthaommkarnath@gmail.com>
2026-05-28 12:16:34 +0530

    Initial Lingua app setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white px-6">
+      <Text className="mt-6 text-center text-4xl font-semibold text-purple-600">
+        Lingua 
+      </Text>
+    </View>
+  );
+}
